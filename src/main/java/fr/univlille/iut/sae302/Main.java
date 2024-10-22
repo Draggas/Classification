6a9e7f51fc9da09f99e0616cb922f20bb450a97d
Enzo Lamour <gradlar@MacBook-Pro-de-Enzo.local>
2024-10-22 19:20:15 +0200

    correction de bug et app viable

--- src/main/java/fr/univlille/iut/sae302/Main.java
+++ src/main/java/fr/univlille/iut/sae302/Main.java
@@ -17,9 +17,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws IOException {
         List<FormatDonneeBrutIris> listBrutIris = ChargementDonneesUtilIris.charger("data/iris.csv");
         List<Iris> listIris = new ArrayList<>();
-//        for (FormatDonneeBrutIris brut : listBrutIris) {
-  //          listIris.add(new Iris(brut.getSepal_length(), brut.getSepal_width(), brut.getPetal_length(), brut.getPetal_width(), brut.getVariety()));
-    //    }
+        for (FormatDonneeBrutIris brut : listBrutIris) {
+            listIris.add(new Iris(brut.getSepal_length(), brut.getSepal_width(), brut.getPetal_length(), brut.getPetal_width(), brut.getVariety()));
+        }
         Systeme systeme = new Systeme(listIris);
 
     }
