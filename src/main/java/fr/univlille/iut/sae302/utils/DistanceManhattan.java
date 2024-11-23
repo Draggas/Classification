9b922e47711ccc06505b9c0f8b99d89c41b7b0b6
Martin Randoux <martin.randoux.etu@univ-lille.fr>
2024-11-23 19:08:49 +0100

    ajout des tests manhattan et début du test methodeknn

--- src/main/java/fr/univlille/iut/sae302/utils/DistanceManhattan.java
+++ src/main/java/fr/univlille/iut/sae302/utils/DistanceManhattan.java
@@ -1,16 +1,15 @@
 package fr.univlille.iut.sae302.utils;
 
 import fr.univlille.iut.sae302.Iris;
-import fr.univlille.iut.sae302.MethodeKnn;
 
 public class DistanceManhattan implements Distance {
 
     @Override
     public double distance(Iris iris1, Iris iris2) {
-        double petalLengthDiff = Math.abs(iris1.getPetalLength().doubleValue() - iris2.getPetalLength().doubleValue()) / MethodeKnn.amplitudePetalLength;
-        double petalWidthDiff = Math.abs(iris1.getPetalWidth().doubleValue() - iris2.getPetalWidth().doubleValue()) / MethodeKnn.amplitudePetalWidth;
-        double sepalLengthDiff = Math.abs(iris1.getSepalLength().doubleValue() - iris2.getSepalLength().doubleValue()) / MethodeKnn.amplitudeSepalLength;
-        double sepalWidthDiff = Math.abs(iris1.getSepalWidth().doubleValue() - iris2.getSepalWidth().doubleValue()) / MethodeKnn.amplitudeSepalWidth;
+        double petalLengthDiff = Math.abs(iris1.getPetalLength().doubleValue() - iris2.getPetalLength().doubleValue());
+        double petalWidthDiff = Math.abs(iris1.getPetalWidth().doubleValue() - iris2.getPetalWidth().doubleValue());
+        double sepalLengthDiff = Math.abs(iris1.getSepalLength().doubleValue() - iris2.getSepalLength().doubleValue());
+        double sepalWidthDiff = Math.abs(iris1.getSepalWidth().doubleValue() - iris2.getSepalWidth().doubleValue());
 
         return petalLengthDiff + petalWidthDiff + sepalLengthDiff + sepalWidthDiff;
     }
