31347a4f8bc6d7c1848c9507d26e1e122de54792
Martin Randoux <martin.randoux.etu@univ-lille.fr>
2024-11-26 14:40:03 +0100

    optimisation et ajout dernier test methodeKnn

--- src/test/java/fr/univlille/iut/sae302/TestMethodeKnn.java
+++ src/test/java/fr/univlille/iut/sae302/TestMethodeKnn.java
@@ -1,6 +1,9 @@
 package fr.univlille.iut.sae302;
 
+import fr.univlille.iut.sae302.utils.Distance;
+import fr.univlille.iut.sae302.utils.DistanceEuclidienne;
 import fr.univlille.iut.sae302.utils.DistanceManhattan;
+import fr.univlille.iut.sae302.utils.DistanceManhattanNormalisee;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,23 +17,26 @@ public class TestMethodeKnn{
     private Iris iris3;
     private Iris iris4;
     private Iris iris5;
+    private Iris iris6;
     private ArrayList<Iris> list;
     private Data<Iris> data;
     private MethodeKnn mKnn;
 
     @BeforeEach
     public void init(){
-        iris1 = new Iris(1.1, 1.2, 1.3, 1.4, "Setosa");
-        iris2 = new Iris(2.1, 4.2, 2.0, 2.4, "Setosa");
-        iris3 = new Iris(1.1, 2.2, 2.3, 1.4, "Setosa");
-        iris4 = new Iris(1.5, 3.5, 1.4, 1.7, "Setosa");
-        iris5 = new Iris(1.9, 2.7, 1.7, 2.0, "Setosa");
+        iris1 = new Iris(5.1, 3.5, 1.4, 0.2, "Setosa");
+        iris2 = new Iris(4.9, 3.0, 1.4, 0.2, "Setosa");
+        iris3 = new Iris(7.0, 3.2, 4.7, 1.4, "Versicolor");
+        iris4 = new Iris(6.4, 3.2, 4.5, 1.5, "Versicolor");
+        iris5 = new Iris(6.3, 3.3, 6.0, 2.5, "Virginica");
+        iris6 = new Iris(5.8, 2.7, 5.1, 1.9, "Virginica");
         list = new ArrayList<>();
         list.add(iris1);
         list.add(iris2);
         list.add(iris3);
         list.add(iris4);
         list.add(iris5);
+        list.add(iris6);
         data = new Data<>(list);
         mKnn = new MethodeKnn(data);
     }
@@ -42,22 +48,22 @@ public class TestMethodeKnn{
 
     @Test
     public void testGetSepalWidthMin(){
-        assertEquals(1.2, mKnn.getSepalWidthMin());
+        assertEquals(2.7, mKnn.getSepalWidthMin());
     }
 
     @Test
     public void testGetSepalLengthMin(){
-        assertEquals(1.1, mKnn.getSepalLengthMin());
+        assertEquals(4.9, mKnn.getSepalLengthMin());
     }
 
     @Test
     public void testGetPetalWidthMin(){
-        assertEquals(1.4, mKnn.getPetalWidthMin());
+        assertEquals(0.2, mKnn.getPetalWidthMin());
     }
 
     @Test
     public void testGetPetalLengthMin(){
-        assertEquals(1.3, mKnn.getPetalLengthMin());
+        assertEquals(1.4, mKnn.getPetalLengthMin());
     }
 
     @Test
@@ -83,33 +89,34 @@ public class TestMethodeKnn{
     @Test
     public void testCalculerAmplitudes(){
         mKnn.calculerAmplitudes();
-        assertEquals(1.0, MethodeKnn.amplitudePetalLength, 0.0001);
-        assertEquals(1.0, MethodeKnn.amplitudePetalWidth, 0.0001);
-        assertEquals(1.0, MethodeKnn.amplitudeSepalLength, 0.0001);
-        assertEquals(3.0, MethodeKnn.amplitudeSepalWidth, 0.0001);
+        assertEquals(4.6, MethodeKnn.amplitudePetalLength, 0.01);
+        assertEquals(2.3, MethodeKnn.amplitudePetalWidth, 0.01);
+        assertEquals(2.1, MethodeKnn.amplitudeSepalLength, 0.01);
+        assertEquals(0.8, MethodeKnn.amplitudeSepalWidth, 0.01);
     }
 
     @Test
-    public void testGetKPlusProchesVoisinsUnVoisin(){
-        Iris[] resultatAttendu = new Iris[]{iris1};
-        Iris[] resultat = mKnn.getKPlusProchesVoisins(1, iris3, new DistanceManhattan());
-
-        testMemeContenueListe(resultatAttendu, resultat);
+    public void testPPVUnVoisinDistanceManhattan(){
+        testGetKPlusProchesVoisins(new Iris[]{iris4}, 1, iris3, new DistanceManhattan());
     }
 
     @Test
-    public void testGetKPlusProchesVoisinsDeuxVoisin(){
-        Iris[] resultatAttendu = new Iris[]{iris1, iris5};
-        Iris[] resultat = mKnn.getKPlusProchesVoisins(2, iris3, new DistanceManhattan());
-
-        testMemeContenueListe(resultatAttendu, resultat);
+    public void testPPVTroisVoisinDistanceManhattan(){
+        testGetKPlusProchesVoisins(new Iris[]{iris4, iris6, iris5}, 3, iris3, new DistanceManhattan());
     }
 
     @Test
-    public void testGetKPlusProchesVoisinsTroisVoisin(){
-        Iris[] resultatAttendu = new Iris[]{iris1, iris5, iris4};
-        Iris[] resultat = mKnn.getKPlusProchesVoisins(3, iris3, new DistanceManhattan());
+    public void testPPVUnVoisinDistanceEuclidienne(){
+        testGetKPlusProchesVoisins(new Iris[]{iris4}, 1, iris3, new DistanceEuclidienne());
+    }
 
+    @Test
+    public void testPPVTroisVoisinDistanceEuclidienne(){
+        testGetKPlusProchesVoisins(new Iris[]{iris4, iris6, iris5}, 3, iris3, new DistanceEuclidienne());
+    }
+
+    public void testGetKPlusProchesVoisins(Iris[] resultatAttendu, int k, Iris cible, Distance distance) {
+        Iris[] resultat = mKnn.getKPlusProchesVoisins(k, cible, distance);
         testMemeContenueListe(resultatAttendu, resultat);
     }
 
@@ -118,5 +125,25 @@ public class TestMethodeKnn{
         for (int i=0; i<resultatAttendu.length; i++){
             assertEquals(resultatAttendu[i], resultat[i]);
         }
+    }
+
+    @Test
+    public void testClassifierIrisUnVoisinDistanceManhattan(){
+        assertEquals("Versicolor", mKnn.classifierIris(1, iris3, new DistanceManhattan()));
+    }
+
+    @Test
+    public void testClassifierIrisTroisVoisinDistanceManhattan(){
+        assertEquals("Virginica", mKnn.classifierIris(3, iris3, new DistanceManhattan()));
+    }
+
+    @Test
+    public void testClassifierIrisUnVoisinDistanceEuclidienne(){
+        assertEquals("Versicolor", mKnn.classifierIris(1, iris3, new DistanceEuclidienne()));
+    }
+
+    @Test
+    public void testClassifierIrisTroisVoisinDistanceEuclidienne(){
+        assertEquals("Virginica", mKnn.classifierIris(3, iris3, new DistanceEuclidienne()));
     }
 }
