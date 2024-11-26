655470398ea19ce1a9332d70b36eba205613288c
Martin Randoux <martin.randoux.etu@univ-lille.fr>
2024-11-26 13:19:15 +0100

    fix test utilisant knn

--- src/test/java/fr/univlille/iut/sae302/TestMethodeKnn.java
+++ src/test/java/fr/univlille/iut/sae302/TestMethodeKnn.java
@@ -13,6 +13,7 @@ public class TestMethodeKnn{
     private Iris iris2;
     private Iris iris3;
     private ArrayList<Iris> list;
+    MethodeKnn mKnn;
 
     @BeforeEach
     public void init(){
@@ -23,32 +24,31 @@ public class TestMethodeKnn{
         list.add(iris1);
         list.add(iris2);
         list.add(iris3);
-        MethodeKnn mKnn = new MethodeKnn(new Data<>(list));
-        MethodeKnn.calculerAmplitudes();
+        mKnn = new MethodeKnn(new Data<>(list));
     }
 
     @Test
     public void testGetData(){
-        assertEquals(list, MethodeKnn.getDatas().getEData());
+        assertEquals(list, mKnn.getDatas().getEData());
     }
 
     @Test
     public void testGetSepalWidthMin(){
-        assertEquals(1.2, MethodeKnn.getSepalWidthMin());
+        assertEquals(1.2, mKnn.getSepalWidthMin());
     }
 
     @Test
     public void testGetSepalLengthMin(){
-        assertEquals(1.1, MethodeKnn.getSepalLengthMin());
+        assertEquals(1.1, mKnn.getSepalLengthMin());
     }
 
     @Test
     public void testGetPetalWidthMin(){
-        assertEquals(1.4, MethodeKnn.getPetalWidthMin());
+        assertEquals(1.4, mKnn.getPetalWidthMin());
     }
 
     @Test
     public void testGetPetalLengthMin(){
-        assertEquals(1.3, MethodeKnn.getPetalLengthMin());
+        assertEquals(1.3, mKnn.getPetalLengthMin());
     }
 }
